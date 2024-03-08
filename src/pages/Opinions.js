109f2032b37fb0d8c83882fad0281318edd77ee3
Solideo Putra Zendrato <solidzendrato@gmail.com>
2024-03-08 23:57:33 +0100

    modified opinions page

--- src/pages/Opinions.js
+++ src/pages/Opinions.js
@@ -27,43 +27,48 @@ function Opinions() {
     fetchingOpinions();
   }, [title])
 
-return(
-    <div className="opinions-main-container">
-      
-      <div className="input-container">
-        {user ? <CreateOpinion /> : <h3>Please login to post your opinion click <Link to="/login">here</Link></h3>}
-      </div>
-      
-      <div className="input-filter-container">
-        <input
-          type="text"
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-          placeholder="Search by title"
-        />
-      </div>
-
-      <div className="opinions-container">
-        {opinions.map(elementOfOpinions => (
-          <div key={elementOfOpinions._id} className="sub-opinions-container">
-            <div className="author-opinion-container">
-              <img src={elementOfOpinions.authorOpinion.picture} alt="img" />
-              <div>
-                <p>Author: {elementOfOpinions.authorOpinion.name}</p>
-                {/* <p>{elementOfOpinions.authorOpinion.profession}</p>  */}
-                <p>{new Date(elementOfOpinions.createdAt).toLocaleDateString("en", { day: "2-digit", month: "long", year: "numeric", hour: "2-digit", minute: "2-digit" })}</p>
-                <hr/>
+if(opinions.length === null  ) {
+  <h3>Loading...</h3>
+} else {
+  return(
+      <div className="opinions-main-container">
+        
+        <div className="input-container">
+          {user ? <CreateOpinion /> : <h3>Please login to post your opinion click <Link to="/login">here</Link></h3>}
+        </div>
+        
+        <div className="input-filter-container">
+          <input
+            type="text"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            placeholder="Search by title"
+          />
+        </div>
+  
+        <div className="opinions-container">
+          {opinions.map(elementOfOpinions => (
+            <div key={elementOfOpinions._id} className="sub-opinions-container">
+              <div className="author-opinion-container">
+                <img src={elementOfOpinions.authorOpinion.picture} alt="img" />
+                <div>
+                  <p>Author: {elementOfOpinions.authorOpinion.name}</p>
+                  {/* <p>{elementOfOpinions.authorOpinion.profession}</p>  */}
+                  <p>{new Date(elementOfOpinions.createdAt).toLocaleDateString("en", { day: "2-digit", month: "long", year: "numeric", hour: "2-digit", minute: "2-digit" })}</p>
+                  <hr/>
+                </div>
               </div>
+              <p className="title">{elementOfOpinions.title}</p>
+              <Link to={`/opinions/${elementOfOpinions._id}`}>
+                {elementOfOpinions.picture ? <img src={elementOfOpinions.picture} alt="img" className="main-picture-from-opinion-page"/> : <p>No Picture</p>}
+              </Link>            
             </div>
-            <p className="title">{elementOfOpinions.title}</p>
-            <Link to={`/opinions/${elementOfOpinions._id}`}>
-              {elementOfOpinions.picture ? <img src={elementOfOpinions.picture} alt="img" className="main-picture-from-opinion-page"/> : <p>No Picture</p>}
-            </Link>            
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+}
+
 };
 
 export default Opinions;
