886ccc17892447765f907b90bcc33fed3f93019d
Solideo Putra Zendrato <solidzendrato@gmail.com>
2024-03-09 20:42:55 +0100

    modified opinion details

--- src/pages/OpinionDetails.js
+++ src/pages/OpinionDetails.js
@@ -1,3 +1,124 @@
+// import axios from "axios";
+// import { useContext, useEffect, useState } from "react";
+// import { Link, useParams } from "react-router-dom";
+// import Comments from "./Comments";
+// import CreateComment from "../components/CreateComment";
+// import { AuthContext } from "../context/auth.context";
+
+// function OpinionDetails() {
+//   const API_URL = process.env.REACT_APP_SERVER_URL;
+//   const {opinionId} = useParams();
+//   const storedToken = localStorage.getItem("authToken");
+//   const {user} = useContext(AuthContext)
+//   const [opinion, setOpinion] = useState("");
+//   const [opinionOwnerId, setOpinionOwnerId] = useState("");
+  
+//   const getCounts = (key) => {
+//     const savedCount = localStorage.getItem(`${key}-${opinionId}`);
+//     return savedCount !== null ? parseInt(savedCount, 10) : 0;
+//   };
+
+//   const [countUp, setCountUp] = useState(() => getCounts('countUp'));
+//   const [countDown, setCountDown] = useState(() => getCounts('countDown'));
+
+//   const [hasVoted, setHasVoted] = useState(() => {
+//     const votes = JSON.parse(localStorage.getItem(`votes-${user?._id}`) || "{}");
+//     return votes[opinionId] !== undefined;
+//   });
+
+//   useEffect(() => {
+//     function fetchingOpinionId() {
+//       axios.get(`${API_URL}/api/opinions/${opinionId}`, {headers: {Authorization: `Bearer ${storedToken}`}})
+//         .then(gettingOpinionId => {
+//           if(gettingOpinionId.data && gettingOpinionId.data.authorOpinion) {
+//             setOpinionOwnerId(gettingOpinionId.data.authorOpinion._id)
+//           }
+//           setOpinion(gettingOpinionId.data)
+//         })
+//         .catch(e => console.log("error getting opinion Id", e))
+//     };
+
+//     fetchingOpinionId();
+//   }, []);
+
+//   useEffect(() => {
+//     localStorage.setItem(`countUp-${opinionId}`, countUp);
+//     localStorage.setItem(`countDown-${opinionId}`, countDown);
+//     // Update the votes record in localStorage
+//     const votes = JSON.parse(localStorage.getItem(`votes-${user?._id}`) || "{}");
+//     if (hasVoted) {
+//       votes[opinionId] = true;
+//       localStorage.setItem(`votes-${user?._id}`, JSON.stringify(votes));
+//     }
+//   }, [countUp, countDown, opinionId, user?._id, hasVoted]);
+
+//   const increment = () => {
+//     if (user && !hasVoted) {
+//       setCountUp(prevCount => prevCount + 1);
+//       setHasVoted(true);
+//     }
+//   };
+
+//   const decrement = () => {
+//     if (user && !hasVoted) {
+//       setCountDown(prevCount => prevCount + 1);
+//       setHasVoted(true);
+//     }
+//   };
+
+//   if(opinion === "") {
+//     return <p>Loading</p>
+//   } else {
+//     return(
+//       <div className="opinion-details-container">
+//         <div className="opinion-author-container">
+          
+//           <img src={opinion.authorOpinion.picture} alt="img" />
+//           <div>
+//             <p>Author: {opinion.authorOpinion.name}</p>
+//             <p>{new Date(opinion.createdAt).toLocaleDateString("en", { day: "2-digit", month: "long", year: "numeric", hour: "2-digit", minute: "2-digit" })}</p>
+//             <p>{opinion.authorOpinion.profession}</p>
+//             <br /> 
+//             <hr/>
+//           </div>
+//         </div>
+        
+//         <div className="opinion-content-container">
+//           <div className="sub-opinion-title-and-picture">
+//             <p className="title">{opinion.title}</p>
+//             {opinion.picture ? <img src={opinion.picture} alt="img" className="main-image"/> : <p>No Picture</p>}
+//           </div>
+
+//           <div className="sub-opinion-body">
+//             <p>{opinion.body}</p>
+//             <div className="agree-disagree-container">  
+//               <button onClick={increment}><p>Agree {countUp}</p></button>
+//               <button onClick={decrement}>Disagree {countDown}</button>
+//             </div>
+//             {user && user._id === opinionOwnerId 
+//             ? 
+//             <button><Link to={`/opinions/edit/${opinion._id}`}>Edit Opinion</Link></button>
+//             :
+//             <></>
+//             }
+//             <br />
+//             {user ? <CreateComment/> : <h3>Please login to leave the comment! <Link to="/login">here</Link></h3>}
+            
+//             <div className="opinion-comments-container">
+//               {opinion && opinion.comments.map(comment => (
+//                 <Comments key={comment._id} {...comment} />
+//               ))}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+
+// };
+
+// export default OpinionDetails;
+
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
@@ -7,64 +128,102 @@ import { AuthContext } from "../context/auth.context";
 
 function OpinionDetails() {
   const API_URL = process.env.REACT_APP_SERVER_URL;
-  const {opinionId} = useParams();
+  const { opinionId } = useParams();
   const storedToken = localStorage.getItem("authToken");
-  const {user} = useContext(AuthContext)
-  const [opinion, setOpinion] = useState("");
+  const { user } = useContext(AuthContext);
+  const [opinion, setOpinion] = useState(null);
   const [opinionOwnerId, setOpinionOwnerId] = useState("");
 
+  const getCounts = (key) => {
+    const savedCount = localStorage.getItem(`${key}-${opinionId}`);
+    return savedCount !== null ? parseInt(savedCount, 10) : 0;
+  };
+
+  const [countUp, setCountUp] = useState(() => getCounts('countUp'));
+  const [countDown, setCountDown] = useState(() => getCounts('countDown'));
+
+  const [voteType, setVoteType] = useState(() => {
+    const votes = JSON.parse(localStorage.getItem(`votes-${user?._id}`) || "{}");
+    return votes[opinionId] || null;
+  });
+
   useEffect(() => {
-    function fetchingOpinionId() {
-      axios.get(`${API_URL}/api/opinions/${opinionId}`, {headers: {Authorization: `Bearer ${storedToken}`}})
-        .then(gettingOpinionId => {
-          if(gettingOpinionId.data && gettingOpinionId.data.authorOpinion) {
-            setOpinionOwnerId(gettingOpinionId.data.authorOpinion._id)
-          }
-          setOpinion(gettingOpinionId.data)
-        })
-        .catch(e => console.log("error getting opinion Id", e))
-    };
+    axios.get(`${API_URL}/api/opinions/${opinionId}`, {
+      headers: { Authorization: `Bearer ${storedToken}` },
+    })
+      .then((response) => {
+        const fetchedOpinion = response.data;
+        if (fetchedOpinion && fetchedOpinion.authorOpinion) {
+          setOpinionOwnerId(fetchedOpinion.authorOpinion._id);
+        }
+        setOpinion(fetchedOpinion);
+      })
+      .catch((error) => console.log("error getting opinion Id", error));
+  }, [API_URL, opinionId, storedToken]);
 
-    fetchingOpinionId();
-  }, []);
+  useEffect(() => {
+    localStorage.setItem(`countUp-${opinionId}`, countUp.toString());
+    localStorage.setItem(`countDown-${opinionId}`, countDown.toString());
+    const votes = JSON.parse(localStorage.getItem(`votes-${user?._id}`) || "{}");
+    if (voteType) {
+      votes[opinionId] = voteType;
+      localStorage.setItem(`votes-${user?._id}`, JSON.stringify(votes));
+    }
+  }, [countUp, countDown, opinionId, user?._id, voteType]);
 
-  if(opinion === "") {
-    return <p>Loading</p>
+  const changeVote = (newVoteType) => {
+    if (user) {
+      const oppositeVoteType = newVoteType === 'up' ? 'down' : 'up';
+      const isChangingVote = voteType && voteType !== newVoteType;
+      
+      if (isChangingVote || voteType === null) {
+        setVoteType(newVoteType);
+        if (newVoteType === 'up') {
+          setCountUp(prevCount => prevCount + 1);
+          if (isChangingVote) setCountDown(prevCount => prevCount - 1);
+        } else {
+          setCountDown(prevCount => prevCount + 1);
+          if (isChangingVote) setCountUp(prevCount => prevCount - 1);
+        }
+      }
+    }
+  };
+
+  if (opinion === null) {
+    return <p>Loading...</p>;
   } else {
-    return(
+    return (
       <div className="opinion-details-container">
         <div className="opinion-author-container">
-          <img src={opinion.authorOpinion.picture} alt="img" />
+          <img src={opinion.authorOpinion.picture} alt="Author" />
           <div>
             <p>Author: {opinion.authorOpinion.name}</p>
             <p>{new Date(opinion.createdAt).toLocaleDateString("en", { day: "2-digit", month: "long", year: "numeric", hour: "2-digit", minute: "2-digit" })}</p>
             <p>{opinion.authorOpinion.profession}</p>
-            <br /> 
-            <hr/>
+            <hr />
           </div>
         </div>
         
         <div className="opinion-content-container">
           <div className="sub-opinion-title-and-picture">
             <p className="title">{opinion.title}</p>
-            {opinion.picture ? <img src={opinion.picture} alt="img" className="main-image"/> : <p>No Picture</p>}
+            {opinion.picture ? <img src={opinion.picture} alt="Opinion" className="main-image" /> : <p>No Picture Available</p>}
           </div>
 
           <div className="sub-opinion-body">
             <p>{opinion.body}</p>
-            {user && user._id === opinionOwnerId 
-            ? 
-            <button>
-              <Link to={`/opinions/edit/${opinion._id}`}>Edit Opinion</Link>
-            </button>
-            :
-            <></>
-            }
+            <div className="agree-disagree-container">  
+              <button onClick={() => changeVote('up')}><p>Agree {countUp}</p></button>
+              <button onClick={() => changeVote('down')}>Disagree {countDown}</button>
+            </div>
+            {user && user._id === opinionOwnerId && (
+              <button><Link to={`/opinions/edit/${opinion._id}`}>Edit Opinion</Link></button>
+            )}
             <br />
-            {user ? <CreateComment/> : <h3>Please login to leave the comment! <Link to="/login">here</Link></h3>}
+            {user ? <CreateComment /> : <h3>Please login to leave a comment! <Link to="/login">Login here</Link></h3>}
             
             <div className="opinion-comments-container">
-              {opinion && opinion.comments.map(comment => (
+              {opinion.comments && opinion.comments.map(comment => (
                 <Comments key={comment._id} {...comment} />
               ))}
             </div>
@@ -73,7 +232,6 @@ function OpinionDetails() {
       </div>
     );
   }
-
-};
+}
 
 export default OpinionDetails;
